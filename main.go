24aaa0f2a6c413dae07446114a6572e410a9d330
gocs <27877342+gocs@users.noreply.github.com>
2024-10-12 19:12:23 +0800

    update port

--- main.go
+++ main.go
@@ -3,19 +3,25 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/gocs/map/ws"
 )
 
 func main() {
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "80"
+	}
+
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	http.HandleFunc("/updaterws", ws.UpdaterWS())
 	http.HandleFunc("/favicon.ico", func(w http.ResponseWriter, r *http.Request) {
 		http.ServeFile(w, r, "./static/xqcLewd.ico")
 	})
 
-	log.Println("Listening on :3131...")
-	if err := http.ListenAndServe(":3131", nil); err != nil {
+	log.Printf("Listening on :%s...\n", port)
+	if err := http.ListenAndServe(":"+port, nil); err != nil {
 		log.Fatal(err)
 	}
 }
